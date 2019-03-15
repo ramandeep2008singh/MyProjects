100202d9dd45ca4615deb13a7020801eb351ed58
“ramandeep2008singh@gmail.com” <ramandeep2008singh@gmail.com>
2019-03-15 17:43:11 +0530

    Added dependencies to POM.xml file

--- MyProjects/src/main/java/com/practice/util/Constants.java
+++ MyProjects/src/main/java/com/practice/util/Constants.java
@@ -6,5 +6,30 @@ package com.practice.util;
  *
  */
 public class Constants {
+	
+	/**
+	 * Constant for Browser
+	 */
+	public static final String BROWSER = "browser";
+	
+	/**
+	 * Constant for Chrome
+	 */
+	public static final String CHROME = "chrome";
+	
+	/**
+	 * Constant for Firefox
+	 */
+	public static final String FIREFOX = "firefox";
+	
+	/**
+	 * Constant for InternetExplorer
+	 */
+	public static final String IE = "ie";
+	
+	/**
+	 * Constant for Edge
+	 */
+	public static final String EDGE = "edge";
 
 }
--- MyProjects/src/main/java/com/practice/util/UiTestUtil.java
+++ MyProjects/src/main/java/com/practice/util/UiTestUtil.java
@@ -1,11 +1,37 @@
 package com.practice.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 /**
  * UI test util methods
+ * 
  * @author ramandeep.singh.nagi
  *
  */
 public class UiTestUtil {
+
+	/**
+	 * variable for properties
+	 */
+	Properties properties = null;
+
+	/**
+	 * variable for file input stream
+	 */
+	FileInputStream fis;
+	
+	/**
+	 * Initializing Object for WebDriver 
+	 */
+	WebDriver driver = null;
 
 	/**
 	 * Method to login into portal
@@ -18,7 +44,49 @@ public class UiTestUtil {
 	 * Method to setup the browser, properties file ...
 	 */
 	public void setup() {
+		
+		if (properties.getProperty(Constants.BROWSER).equalsIgnoreCase(Constants.CHROME)) {
+			driver = new ChromeDriver();
+			// TODO: add code here
+		} else if (properties.getProperty(Constants.BROWSER).equalsIgnoreCase(Constants.FIREFOX)) {
+			driver = new FirefoxDriver();
+			// TODO: add code here			
+		} else if (properties.getProperty(Constants.BROWSER).equalsIgnoreCase(Constants.IE)) {
+			driver = new InternetExplorerDriver();
+			// TODO: add code here
+		} else if (properties.getProperty(Constants.BROWSER).equalsIgnoreCase(Constants.EDGE)) {
+			driver = new EdgeDriver();
+			// TODO: add code here
+		}
 
+	}
+
+	/**
+	 * loading properties file
+	 */
+	public void loadUiProperties() {
+		properties = new Properties();
+		try {
+			fis = new FileInputStream(System.getProperty("user.dir")
+					+ "//Ramandeep2008Singh//src//main//resources//properties//UiTests.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * loading properties file
+	 */
+	public void loadApiProperties() {
+		properties = new Properties();
+		try {
+			fis = new FileInputStream(System.getProperty("user.dir")
+					+ "//Ramandeep2008Singh//src//main//resources//properties//ApiTests.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -48,7 +116,7 @@ public class UiTestUtil {
 	public void takeScreenshot() {
 
 	}
-	
+
 	/**
 	 * Method for retry failure cases
 	 */
