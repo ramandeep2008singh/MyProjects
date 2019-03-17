be1136d7af7fc3604d6cbbe2662f649a0b8aac12
“ramandeep2008singh@gmail.com” <ramandeep2008singh@gmail.com>
2019-03-17 14:00:40 +0530

    added api testing casaes

--- MyProjects/src/main/java/com/practice/StepDefinitions/ApiStepDefinition.java
+++ MyProjects/src/main/java/com/practice/StepDefinitions/ApiStepDefinition.java
@@ -1,17 +1,44 @@
 package com.practice.StepDefinitions;
 
-import com.practice.util.ApiTestUtil;
+import java.io.IOException;
+import java.util.Set;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.ValidationMessage;
 
 /**
- * This class contains tests from API
- * @author ramandeep.singh.nagi
  *
  */
-public class ApiStepDefinition extends ApiTestUtil {
-	
-	/**
-	 * 1111
-	 * Method to setup the http
-	 */
+public class ApiStepDefinition {
 
+	@Test
+	public void restTest() throws ClientProtocolException, IOException {
+		// Given
+		// String name = RandomStringUtils.randomAlphabetic(8);
+		HttpUriRequest request = new HttpGet("http://dummy.restapiexample.com/api/v1/employees");
+
+		// When
+		CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		// Then
+		// assertThat(httpResponse.getStatusLine().getStatusCode(),
+		// equalTo(HttpStatus.SC_NOT_FOUND));
+		JsonNode jsonNode = new ObjectMapper().readTree(httpResponse.getEntity().getContent());
+		//String jsonResponse = EntityUtils.toString(httpResponse.getEntity());
+		JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance();
+
+		JsonSchema schema = schemaFactory.getSchema(getClass().getResourceAsStream("/other/event-schema.json"));
+		// Validate the JSON against the schema.
+		Set<ValidationMessage> validationErrors = schema.validate(jsonNode);
+	}
 }
